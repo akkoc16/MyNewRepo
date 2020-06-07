7f28b4e1246e9127a7f2e53feeb2404528d6fe28
akkoc16 <m.enver.akkoc@gmail.com>
2020-06-07 11:46:14 +0300

    Flatlist

---
+++ src/List.js
@@ -0,0 +1,42 @@
+import React, {Component, version} from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+    Dimensions,
+    Image,
+    TextInput,
+    FlatList,
+  } from 'react-native';
+import ListItem from './components/ListItem'
+
+
+const mockData = [
+    {id: 0, todo: "Ev temizlenecek", isDone: false},
+    {id: 1, todo: "Çöp atılacak", isDone: false},
+    {id: 2, todo: "Alışveriş yapılacak", isDone: true}
+
+]
+
+const List = props => {
+    return (
+        <SafeAreaView style = {{flex: 1}}>
+            
+            <View style={{flex:1}}>
+
+                <FlatList
+                    keyExtractor = {(item, index) => index.toString()} 
+                    data = {mockData}
+                    renderItem = {({ item }) => <ListItem myData={item} />}>
+                </FlatList>
+
+            </View>
+          
+        </SafeAreaView>
+    )
+
+}
+export default List
---
+++ src/components/ListItem.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { View, Text, StyleSheet, SafeAreaView, ShadowPropTypesIOS} from 'react-native'
+import List from '../List'
+
+const ListItem = props => {
+    return (
+        <View style={[styles.container, {backgroundColor: props.myData.isDone ? '#dcedc8' : '#ff8a65'}]}>
+            <View style= {{flexDirection: 'row', alignItems: 'center'}}>
+                <View style={styles.dot}></View>
+                <Text style = {styles.text}>{props.myData.todo}</Text>
+            </View>
+            
+            <Text>
+                {
+                    props.myData.isDone ? "Yapıldı!" : "Yapılmadı!"
+                }
+            </Text>
+            
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#dcedc8',
+        margin: 5,
+        padding: 5,
+        borderRadius: 10,
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    text: {
+        fontSize: 20,
+        marginLeft: 10,
+    },
+    dot: {
+        width: 10,
+        height: 10,
+        backgroundColor: 'black',
+        borderRadius: 20
+    }
+
+})
+
+export default ListItem
